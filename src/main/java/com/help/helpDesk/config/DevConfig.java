d9272242d278ecb652bb501df416c9a37bbea452
Giovanna Marozzi <giovanna.machado99@hotmail.com>
2023-09-18 17:54:04 -0300

    Development people created

---
+++ src/main/java/com/help/helpDesk/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.help.helpDesk.config;
+
+import com.help.helpDesk.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService service;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+
+    @Bean
+    public boolean instanciaDB(){
+
+        if (value.equals("create")){
+            this.service.instanciaDB();
+        }
+        return false;
+    }
+}
