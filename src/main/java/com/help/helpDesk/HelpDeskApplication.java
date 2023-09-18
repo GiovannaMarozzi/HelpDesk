aa6c00add5b61420d68179eb3560cf03068b1335
Giovanna Marozzi <giovanna.machado99@hotmail.com>
2023-09-18 16:47:03 -0300

    test for people created

--- src/main/java/com/help/helpDesk/HelpDeskApplication.java
+++ src/main/java/com/help/helpDesk/HelpDeskApplication.java
@@ -17,30 +17,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class HelpDeskApplication implements CommandLineRunner {
+public class HelpDeskApplication {
 
-	@Autowired
-	private TecnicoRepository tecnicoRepository;
-	@Autowired
-	private ClienteRepository clienteRepository;
-	@Autowired
-	private ChamadoRepository chamadoRepository;
+    public static void main(String[] args) {
+        SpringApplication.run(HelpDeskApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelpDeskApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Tecnico tec1 = new Tecnico(null, "Valdir Cesar", "123.456.789-11", "valdir@email.com", "123", null);
-		tec1.addPerfil(Perfil.AMDMIN);
-
-		Cliente cli1 = new Cliente(null, "Linus Torvalts", "147582635-48", "torvalts@email.com", "teste", null);
-
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
-
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		chamadoRepository.saveAll(Arrays.asList(c1));
-		}
 }
---
+++ src/main/java/com/help/helpDesk/services/DBService.java
@@ -0,0 +1,38 @@
+package com.help.helpDesk.services;
+
+import com.help.helpDesk.domain.Chamado;
+import com.help.helpDesk.domain.Cliente;
+import com.help.helpDesk.domain.Tecnico;
+import com.help.helpDesk.enums.Perfil;
+import com.help.helpDesk.enums.Prioridade;
+import com.help.helpDesk.enums.Status;
+import com.help.helpDesk.repository.ChamadoRepository;
+import com.help.helpDesk.repository.ClienteRepository;
+import com.help.helpDesk.repository.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class DBService {
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+    public void instanciaDB(){
+        Tecnico tec1 = new Tecnico(null, "Valdir Cesar", "123.456.789-11", "valdir@email.com", "123", null);
+        tec1.addPerfil(Perfil.AMDMIN);
+
+        Cliente cli1 = new Cliente(null, "Linus Torvalts", "147582635-48", "torvalts@email.com", "teste", null);
+
+        Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
+
+        tecnicoRepository.saveAll(Arrays.asList(tec1));
+        clienteRepository.saveAll(Arrays.asList(cli1));
+        chamadoRepository.saveAll(Arrays.asList(c1));
+
+    }
+}
